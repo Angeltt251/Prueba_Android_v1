e7ea7272b258e81cbf1de9c21b945827c79a1316
miguel <josepapito71@gmail.com>
2025-11-21 20:46:17 -0300

    PRUEBA ANDRIOD actualizacion 2

---
+++ app/src/main/java/com/example/prueba_android/AdminSQLiteOpenHelper.java
@@ -0,0 +1,30 @@
+package com.example.prueba_android;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE ANIMALES (" +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NOMBRE VARCHAR, " +
+                "DESCRIPCION VARCHAR, " +
+                "TIPO VARCHAR, " +
+                "EDAD INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS ANIMALES");
+        onCreate(db);
+    }
+}
